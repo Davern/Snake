96b539a7c3f3375de589ccda36c019299587d240
Davern <51719544+Davern@users.noreply.github.com>
2021-01-29 15:59:44 -0800

    rechanged main file to get it working

---
+++ main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+import numpy as np 
+
+
+
+class Food(object):
+    def __init__(self):
+        pass
+
+
+class Snake(object):
+    def __init__(self):
+        pass
+
+
+def main():
+    pass
